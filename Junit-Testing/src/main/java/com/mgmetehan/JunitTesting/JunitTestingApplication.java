b20a294965d41eeb719ed88deb540905844ddf33
mgmetehan <mgmetehan@hotmail.com>
2023-02-17 20:11:17 +0300

    init commit

---
+++ Junit-Testing/src/main/java/com/mgmetehan/JunitTesting/JunitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.JunitTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestingApplication.class, args);
+	}
+
+}
